29bf5e90adf97e9591715b7fc0ba3da91c5e640a
jinyong11 <56147995+jinyong11@users.noreply.github.com>
2021-09-24 20:17:16 +0900

    Update UpbitVer2.py

--- UpbitVer2.py
+++ UpbitVer2.py
@@ -14,14 +14,6 @@ def buy_price(ticker):
 
 print(buy_price("KRW-BTC"))
 
-'''
-def sell_price(ticker):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    sell_price = df.iloc[0]['close']
-    return sell_price
-'''
-
 
 def get_start_time(ticker):
     """시작 시간 조회"""
@@ -36,7 +28,17 @@ def get_ma5(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="day", count=6)
     ma5 = df['close'].rolling(5).mean().shift(1).iloc[-1]
     return ma5
-print(get_ma5("KRW-BTC"))
+
+print(get_ma5("KRW-ETH"))
+
+def get_ma10(ticker):
+    """10일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=11)
+    ma10 = df['close'].rolling(10).mean().shift(1).iloc[-1]
+    return ma10
+    
+print(get_ma10("KRW-ETH"))
+
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -72,28 +74,33 @@ while True:
         for ticker in tickers:
             buyprice = buy_price(ticker)
             ma5 = get_ma5(ticker)
+            ma10 = get_ma10(ticker)
             ticker_balance = upbit.get_balance(ticker)
             current_price = get_current_price(ticker)
             krw = upbit.get_balance("KRW")
             print("check start:", ticker, ticker_balance, "/" ,"KRW:", krw)
+            print(buyprice, ma5, ma10)
             if start_time < now < end_time:        
-                print("check ma5:", ticker,ticker_balance, "/", "KRW:", krw)
-                if buyprice > ma5:
+                print("check ma5, ma10:", ticker,ticker_balance, "/", "KRW:", krw)
+                if buyprice > ma5 and ma5 >= ma10:
                     if krw > 500000 and upbit.get_balance(ticker) < 0.001:
                         upbit.buy_market_order(ticker, krw*0.2)
                         print("Buy complete:", ticker, krw*0.2)
                     else : 
                         print("already have one or not enought money")
 
+                elif buyprice > ma5 and ma5 < ma10:
+                    print("ma5 is lower than ma10")
+
                 else:
                     if ticker_balance > 0.001:
                         upbit.sell_market_order(ticker, ticker_balance)
                         print("Sell complete:", ticker, ticker_balance)   
-                print("checked ma5:", ticker, ticker_balance)
+                print("checked ma5, ma10:", ticker, ticker_balance)
                 time.sleep(0.2)
 
             elif end_time < now and ticker_balance > 0.001:
-                    print(now, "monitoring overshoot & drop:", ticker, ticker_balance)
+                    print(now, "monitoring overshoot & drop:", ticker, ticker_balance, "/" , ticker_balance*current_price)
                     if current_price > buyprice*1.25:
                         if ticker_balance*current_price > 450000:
                             upbit.sell_market_order(ticker, ticker_balance*0.5)
